af382fb0e226966ad7d1eeed081b26f4a160b893
bkiesew <bkiesew@gmail.com>
2025-04-02 20:15:33 -0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Event Package Calculator',
+  description: 'Calculate event packages and pricing',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
