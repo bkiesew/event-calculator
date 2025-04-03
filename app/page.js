363655b3e335b523282581f54adf31464f8dd828
bkiesew <bkiesew@gmail.com>
2025-04-02 20:30:02 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import React from 'react';
-import PackageCalculator from '@/components/PackageCalculator';
-
+import PackageCalculator from '../components/PackageCalculator';
 export default function Home() {
   return (
     <main className="min-h-screen p-4 md:p-8">
