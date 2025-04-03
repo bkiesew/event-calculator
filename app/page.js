28013eacfbc57fa35d851cae3c4bba0356d872c9
bkiesew <bkiesew@gmail.com>
2025-04-02 20:16:51 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+"use client";
+import React from 'react';
+import PackageCalculator from '@/components/PackageCalculator';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4 md:p-8">
+      <PackageCalculator />
+    </main>
+  );
+}
